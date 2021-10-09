f5ae4606c94d92a0b59fa3cbdab11a9ff887ea61
Alexander Gorokhov <sashgorokhov@gmail.com>
2021-10-09 22:05:45 +0300

    (fix): Fix windows reference

--- blackbox_fetch/io.py
+++ blackbox_fetch/io.py
@@ -6,7 +6,6 @@ from pathlib import Path
 from typing import List, Optional, Tuple
 
 import serial.tools.list_ports
-import win32api
 from blackbox_fetch import msp
 
 logger = logging.getLogger(__name__)
@@ -68,6 +67,8 @@ class FilesystemWindows(Filesystem):
         return list(drive.glob('*.bbl'))
 
     def get_blackbox_drive(self) -> Optional[Path]:
+        import win32api
+
         drives = list(filter(lambda p: p.exists(), (Path(f'{letter}:/') for letter in string.ascii_uppercase)))
         drive_labels: List[Tuple[Path, str]] = [(drive, str(win32api.GetVolumeInformation(str(drive))[0])) for drive in drives]
 
